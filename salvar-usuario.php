46cc9c2fa0b57fc22f7c495c02dd12c9d9055756
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-04-04 21:42:14 -0300

    feat: :sparkles: Edit logic implemented

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -20,13 +20,40 @@
       }
       break;
     case 'editar':
-      # code...
+      $nome = $_REQUEST["nome"];
+      $email = $_REQUEST["email"];
+      $data_nasc = $_REQUEST["data_nasc"];
+      $id = $_REQUEST["id"];
+      $id = trim($id, '.');
+
+      $sql = "UPDATE usuarios SET nome='{$nome}', email='{$email}', data_nasc='{$data_nasc}' WHERE id=$id";
+
+      $res = $conn->query($sql);
+
+      if ($res == true) {
+        print "<script>alert('Usuário atualizado com sucesso!')</script>";
+        print "<script>location.href='?page=listar'</script>";
+      } else {
+        print "<script>alert('Ocorreu um erro!')</script>";
+        print "<script>location.href='?page=listar'</script>";
+      };
+
       break;
+
     case 'excluir':
-      # code...
-      break;
-    default:
-      # code...
+      $id = $_REQUEST['id'];
+      $sql = "DELETE FROM usuarios WHERE id=$id";
+
+      $res = $conn->query($sql);
+
+      if ($res == true) {
+        print "<script>alert('Usuário excluído com sucesso!')</script>";
+        print "<script>location.href='?page=listar'</script>";
+      } else {
+        print "<script>alert('Ocorreu um erro!')</script>";
+        print "<script>location.href='?page=listar'</script>";
+      };
+
       break;
   }
 
