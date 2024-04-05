17e78ca0fc50315f7c05fbf38d9df1525e06d8c1
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-04-04 21:44:47 -0300

    feat: :sparkles: Table list logic implemented

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,38 +1,45 @@
 <h3 class="d-flex justify-content-center">Listar usuários</h3>
 
-<div class="container">
-  <div class="row mt-5">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Nº</th>
-          <th scope="col">id</th>
-          <th scope="col">nome</th>
-          <th scope="col">email</th>
-          <th scope="col">senha</th>
-          <th scope="col">data_nasc</th>
-          <th scope="col">botões</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
+<?php 
+  $sql = "SELECT * FROM usuarios";
+
+  $res = $conn->query($sql);
+
+  $qtd = $res->num_rows;
+
+  if ($qtd > 0) {
+
+    print "<p class='alert alert-success'>Resultados encontrados: <b>$qtd</b> </p>";
+
+    print "<table class='table table-hover table-striped table-bordered'>";
+    print "<tr>";
+    print "<th>Nº</th>";
+    print "<th>id</th>";
+    print "<th>nome</th>";
+    print "<th>email</th>";
+    print "<th>data_nasc</th>";
+    print "<th>modificar</th>";
+    print "</tr>";
+
+
+    $count = 1;
+    while ($row = $res->fetch_object()) {
+      print "<tr>";
+      print "<td>" . $count . "</td>";
+      print "<td>" . $row->id . "</td>";
+      print "<td>" . $row->nome . "</td>";
+      print "<td>" . $row->email . "</td>";
+      print "<td>" . $row->data_nasc . "</td>";
+      print "<td>
+              <button onclick=\"location.href='?page=editar&id=".$row->id. "';\" class='btn btn-primary'><i class='fas fa-pencil-alt'></i>     editar</button>
+              <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=" . $row->id . "';}else{false;}\" 
+              class='btn btn-danger'>excluir   <i class='fas fa-trash fa-lg'></i></button>
+            </td>";
+      $count++;
+
+    }
+  } else {
+    print "<div class='alert alert-danger'>Nenhum usuário foi encontrado!</div>";
+  }
+
+?>
